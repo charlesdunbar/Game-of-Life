8ef50cb4e8863a815142699582927c5992983c3e
Charles Dunbar <ccdunbar@gmail.com>
2017-10-09 22:31:13 -0500

    Add tests to ensure rules are followed each generation

--- conway_test.go
+++ conway_test.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
+
+func TestNextGeneration(t *testing.T) {
+	b := board{
+		{false, true, true, false, true},
+		{true, false, true, true, false},
+		{false, false, false, false, true},
+		{false, false, true, true, true},
+		{true, false, true, false, false},
+	}
+	fmt.Println(b)
+
+	b = runRules(&b)
+
+	fmt.Println(b)
+	// Test cell dying from not enough neighbors
+	if b[4][0] {
+		t.Errorf("Cell[4][0] with no neighbors should die")
+	}
+	// Test cell with 2 neighbors
+	if !b[4][2] {
+		t.Errorf("Cell[4][2] with 2 neighbors should still be alive")
+	}
+	// Test cell dying from too many neighbors
+	if b[3][3] {
+		t.Errorf("Cell[3][3] with 4 neighbors should die")
+	}
+	// Test dead cell becoming alive
+	if !b[3][1] {
+		t.Errorf("Dead cell[3][1] with 3 neighbors should live")
+	}
+
+}
 
 func TestFindNeighbors(t *testing.T) {
 	b := board{
